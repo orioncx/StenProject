169bd39c36541479135ae79a12004a204f4bba08
Artem Ustimov <art@ustimov.org>
2016-02-22 19:19:51 +0300

    Added missing templates

--- base/urls.py
+++ base/urls.py
@@ -3,7 +3,7 @@ from base.api.paginator import  paginator_view
 from base.views import  rent_form_sent
 
 urlpatterns = [
-    url(r'^i18n/', include('django.conf.urls.i18n')),
     url(r'^api/feedbacks/', paginator_view, name='api_feedbacks'),
+    url(r'^i18n/', include('django.conf.urls.i18n')),
     url(r'^rent_form/', rent_form_sent, name='rent_form')
 ]
